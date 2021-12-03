73bc7f4fc8080ab9d7073a382cfddcf210125ab9
Sid Nair <siddharthnair87@outlook.com>
2021-12-03 11:19:38 +0000

    created timer div

---
+++ modal-project/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Ninja Reaction Timer</h1>
+    <button @click="start">Play</button>
+    <Block v-if="isPlaying" :delay="delay" />
+  </div>
+</template>
+
+<script>
+import Block from "./components/Block.vue";
+export default {
+  name: "App",
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      console.log(this.delay);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+}
+</style>
